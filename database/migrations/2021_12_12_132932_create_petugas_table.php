2b143c7b6bd700ee80fb0be35b67fd2c143c7458
HarisAja68 <samsudinharis3@gmail.com>
2021-12-12 20:44:20 +0700

    membuat migration

---
+++ database/migrations/2021_12_12_132932_create_petugas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePetugasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('petugas', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_petugas')->nullable();
+            $table->string('nama_petugas')->nullable();
+            $table->string('no_telp')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('petugas');
+    }
+}
---
+++ database/migrations/2021_12_12_133022_create_santri_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSantriTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('santri', function (Blueprint $table) {
+            $table->id();
+            $table->string('nis')->nullable();
+            $table->string('nama_santri')->nullable();
+            $table->string('alamat')->nullable();
+            $table->string('jenis_kelamin')->nullable();
+            $table->string('no_telp')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('santri');
+    }
+}
